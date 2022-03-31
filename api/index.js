b990554f61472b8559c8d6af3d0a7b6c3788b1ef
Logan Martlew <logan.martlew@gmail.com>
2022-03-31 14:02:32 +1300

    Fixed API port

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.get('/*', (req, res) => {
   response.sendFile(path.resolve(__dirname, './public', 'index.html'));
 });
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`API listening on port ${PORT}...`);
