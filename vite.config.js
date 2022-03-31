0e561bb3c1934257823b1f8229c618b8450e887a
Logan Martlew <logan.martlew@gmail.com>
2022-03-31 13:35:19 +1300

    Setup build for react app

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './client',
+  build: {
+    outDir: '../api/public',
+  },
   plugins: [react()],
 });
