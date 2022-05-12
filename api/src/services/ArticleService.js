19c2507f0879c0217d71e80f761eab72dcca8852
Logan Martlew <logan.martlew@gmail.com>
2022-05-12 12:51:08 +1200

    Added article methods for get, getOne, create

--- api/src/services/ArticleService.js
+++ api/src/services/ArticleService.js
@@ -1,15 +1,43 @@
+const { Article } = require('../models/Article');
+
 class ArticleService {
-  static getArticles() {}
+  static async getArticles() {
+    const articles = await Article.find();
+    return articles;
+  }
 
-  static getSingleArticle(id) {}
+  static async getSingleArticle(id) {
+    try {
+      const article = await Article.findById(id);
+      if (!article) return null;
 
-  static searchArticles(q) {}
+      return article;
+    } catch (error) {
+      return null;
+    }
+  }
 
-  static createArticle(articleData) {}
+  static async searchArticles(q) {}
 
-  static updateArticle(newArticle) {}
+  static async createArticle(articleData) {
+    // Validate new article
 
-  static deleteArticle(id) {}
+    const { title, authors, year, doi, sePractice } = articleData;
+
+    const newArticle = new Article();
+    newArticle.title = title;
+    newArticle.authors = authors;
+    newArticle.year = year;
+    newArticle.doi = doi;
+    newArticle.sePractice = sePractice;
+    newArticle.save();
+
+    return newArticle;
+  }
+
+  static async updateArticle(newArticle) {}
+
+  static async deleteArticle(id) {}
 }
 
 module.exports = ArticleService;
