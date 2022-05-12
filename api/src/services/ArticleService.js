6bbdda7ee7cadfd8e49bf805e39430a5a111d88a
Logan Martlew <logan.martlew@gmail.com>
2022-05-12 12:36:44 +1200

    Added stub methods for article service

---
+++ api/src/services/ArticleService.js
@@ -0,0 +1,15 @@
+class ArticleService {
+  static getArticles() {}
+
+  static getSingleArticle(id) {}
+
+  static searchArticles(q) {}
+
+  static createArticle(articleData) {}
+
+  static updateArticle(newArticle) {}
+
+  static deleteArticle(id) {}
+}
+
+module.exports = ArticleService;
