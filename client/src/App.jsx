b8955bf7a20a5bd2dd711125e18b7db26f1596c3
Logan Martlew <logan.martlew@gmail.com>
2022-05-12 13:36:04 +1200

    Added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,7 @@
-import { useEffect } from 'react';
+import AppRoutes from './AppRoutes';
 
 const App = () => {
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/articles`)
-      .then((res) => res.json())
-      .then(console.log);
-  }, []);
-
-  return <div>Hello from client</div>;
+  return <AppRoutes />;
 };
 
 export default App;
