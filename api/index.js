062ee4573fad43b9ee8eb074cfccfc55201b22cd
Logan Martlew <logan.martlew@gmail.com>
2022-05-12 12:16:41 +1200

    Moved functionality to start app from index to loaders

--- api/index.js
+++ api/index.js
@@ -1,42 +1,17 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const { json } = require('body-parser');
-const path = require('path');
-const { connect } = require('mongoose');
+const { getApp } = require('./src/app');
+const loaders = require('./src/loaders');
 
-dotenv.config();
+const startApp = async () => {
+  const app = getApp();
 
-async function connectDB() {
-  try {
-    connect(process.env.MONGO_URI);
-    console.log('Connected to database');
-  } catch (error) {
-    console.log(error);
-    throw new Error('Unable to connect to database');
-  }
-}
+  await loaders(app);
 
-connectDB();
+  const PORT = process.env.PORT || 5000;
 
-const app = express();
+  app.listen(PORT, () => {
+    console.log(`API listening on port ${PORT}...`);
+    console.log(`Live at: http://localhost:${PORT}`);
+  });
+};
 
-app.use(cors());
-app.use(json());
-
-app.use(express.static(path.resolve(__dirname, './public')));
-
-app.get('/api', (_, res) => {
-  res.send('Hello from API');
-});
-
-app.get('/*', (_, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`API listening on port ${PORT}...`);
-  console.log(`Live at: http://localhost:${PORT}`);
-});
+startApp();
---
+++ api/src/loaders.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { json } = require('body-parser');
+const connectDB = require('./db');
+
+const envLoader = async () => {
+  dotenv.config();
+};
+
+const expressLoader = async (app) => {
+  app.use(cors());
+  app.use(json());
+
+  app.use(express.static(path.resolve(__dirname, './public')));
+};
+
+const mongooseLoader = async () => {
+  connectDB();
+};
+
+const apiLoader = async () => {};
+
+const reactLoader = async (app) => {
+  app.get('/*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  });
+};
+
+const loadApp = async (app) => {
+  await envLoader();
+  await expressLoader(app);
+  await mongooseLoader();
+  await apiLoader();
+  await reactLoader(app);
+};
+
+module.exports = loadApp;
