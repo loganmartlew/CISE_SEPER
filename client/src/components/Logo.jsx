dbf66109d2fe361eb0137fae9e5e10c9f2350fe8
Eni090 <89555783+Eni090@users.noreply.github.com>
2022-05-18 15:57:39 +1200

    Formatted code

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -2,18 +2,18 @@ import { styled, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 const LogoText = styled(Typography)(({ theme }) => ({
-	color: theme.palette.primary.main,
-	letterSpacing: 4
+  color: theme.palette.primary.main,
+  letterSpacing: 4,
 }));
 
 const Logo = () => {
-	return (
-		<Link to="/">
-			<LogoText variant="h4" component="h1" fontSize="2.2rem" fontWeight={600}>
-				SPEED
-			</LogoText>
-		</Link>
-	);
+  return (
+    <Link to='/'>
+      <LogoText variant='h4' component='h1' fontSize='2.2rem' fontWeight={600}>
+        SPEED
+      </LogoText>
+    </Link>
+  );
 };
 
 export default Logo;
