36966f2650332f7bd2677c59238d1b6efa3733d0
Eni090 <89555783+Eni090@users.noreply.github.com>
2022-05-18 15:51:44 +1200

    Logo link updated

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,16 +1,19 @@
 import { styled, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const LogoText = styled(Typography)(({ theme }) => ({
-  color: theme.palette.primary.main,
-  letterSpacing: 4,
+	color: theme.palette.primary.main,
+	letterSpacing: 4
 }));
 
 const Logo = () => {
-  return (
-    <LogoText variant='h4' component='h1' fontSize='2.2rem' fontWeight={600}>
-      SPEED
-    </LogoText>
-  );
+	return (
+		<Link to="/">
+			<LogoText variant="h4" component="h1" fontSize="2.2rem" fontWeight={600}>
+				SPEED
+			</LogoText>
+		</Link>
+	);
 };
 
 export default Logo;
