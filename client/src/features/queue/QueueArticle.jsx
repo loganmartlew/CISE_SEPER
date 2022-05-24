3bd242ffd1ebcc04e486efa6c2560ca09afb0d23
Logan Martlew <logan.martlew@gmail.com>
2022-05-25 09:08:25 +1200

    Added sgtyling to queue card

--- client/src/features/queue/QueueArticle.jsx
+++ client/src/features/queue/QueueArticle.jsx
@@ -1,8 +1,31 @@
-import {} from 'react';
+import { Typography, styled } from '@mui/material';
+import Card from '../../components/Card';
 
-const QueueArticle = ({ article }) => {
+const QueueCard = styled(Card)(({ theme, selected }) => ({
+  border: `2px solid ${
+    selected ? theme.palette.primary.main : theme.palette.common.white
+  }`,
+}));
+
+const QueueArticle = ({ article, selected }) => {
   console.log(article);
-  return <div></div>;
+  return (
+    <QueueCard selected={selected}>
+      <Typography variant='h6'>{article.title}</Typography>
+      <Typography fontWeight={600}>Authors:</Typography>
+      <Typography fontStyle='italic'>
+        {article.authors}
+        {' - '}
+        <Typography
+          variant='inherit'
+          fontWeight={600}
+          sx={{ display: 'inline' }}
+        >
+          {article.year}
+        </Typography>
+      </Typography>
+    </QueueCard>
+  );
 };
 
 export default QueueArticle;
--- client/src/features/queue/QueueList.jsx
+++ client/src/features/queue/QueueList.jsx
@@ -1,12 +1,17 @@
+import { Stack } from '@mui/material';
 import QueueArticle from './QueueArticle';
 
 const QueueList = ({ articles }) => {
   return (
-    <div>
-      {articles.map((article) => (
-        <QueueArticle key={article._id} article={article} />
+    <Stack spacing={2}>
+      {articles.map((article, idx) => (
+        <QueueArticle
+          key={article._id}
+          article={article}
+          selected={idx === 0}
+        />
       ))}
-    </div>
+    </Stack>
   );
 };
 
