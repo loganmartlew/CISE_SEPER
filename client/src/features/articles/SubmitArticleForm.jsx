2e0c5c510e12e53dbd0b2e64027572f287ed372e
Logan Martlew <logan.martlew@gmail.com>
2022-05-31 22:07:24 +1200

    Added select for se practices

--- client/src/features/articles/SubmitArticleForm.jsx
+++ client/src/features/articles/SubmitArticleForm.jsx
@@ -1,16 +1,27 @@
-import { useForm } from 'react-hook-form';
-import { Stack, styled } from '@mui/material';
+import { useForm, Controller } from 'react-hook-form';
+import {
+  Select,
+  Stack,
+  MenuItem,
+  styled,
+  FormControl,
+  FormHelperText,
+} from '@mui/material';
 import Button from '../../components/Button';
-import TextInput from '../../components/TextInput';
+import TextInput, {
+  StyledInput,
+  StyledLabel,
+} from '../../components/TextInput';
 
 const Form = styled('form')({});
 
 const SubmitArticleForm = ({ tool, onSubmit, onReject, defaultValues }) => {
   const {
     register,
+    control,
     handleSubmit,
     formState: { errors },
-  } = useForm({ defaultValues });
+  } = useForm({ defaultValues: { ...defaultValues, practice: 'default' } });
 
   return (
     <Form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
@@ -56,6 +67,43 @@ const SubmitArticleForm = ({ tool, onSubmit, onReject, defaultValues }) => {
           error={errors?.doi}
           helperText={errors?.doi?.message}
         />
+
+        {tool && (
+          <Controller
+            name='practice'
+            control={control}
+            defaultValue='default'
+            rules={{
+              required: { value: true, message: 'Practice is required' },
+              validate: (value) =>
+                // eslint-disable-next-line implicit-arrow-linebreak
+                value !== 'default' || 'Please select a practice',
+            }}
+            render={({ field }) => (
+              <FormControl variant='standard'>
+                <StyledLabel htmlFor='practice'>SE Practice</StyledLabel>
+                <Select
+                  {...field}
+                  input={
+                    <StyledInput id='practice' error={!!errors?.practice} />
+                  }
+                >
+                  <MenuItem value='default' disabled hidden>
+                    Please choose an SE practice
+                  </MenuItem>
+                  <MenuItem value='1'>Practice 1</MenuItem>
+                  <MenuItem value='2'>Practice 2</MenuItem>
+                  <MenuItem value='3'>Practice 3</MenuItem>
+                  <MenuItem value='4'>Practice 4</MenuItem>
+                </Select>
+                <FormHelperText error={!!errors?.practice}>
+                  {errors?.practice?.message}
+                </FormHelperText>
+              </FormControl>
+            )}
+          />
+        )}
+
         <Stack direction='row' spacing={2}>
           <Button
             type='submit'
